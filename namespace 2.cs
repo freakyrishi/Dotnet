4a5f8ecd7c67f2504baaccb11a54587a5240aec7
freakyrishi <95204461+freakyrishi@users.noreply.github.com>
2022-02-28 18:56:35 +0530

    Add files via upload

---
+++ namespace 2.cs
@@ -0,0 +1,43 @@
+﻿
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+
+{
+    public class goo
+    {
+        public void sayhello()
+        {
+            Console.WriteLine("Hello Guys");
+        }
+    }
+}
+namespace second
+
+{
+    public class goo
+    {
+        public void sayhello()
+        {
+            Console.WriteLine("Hello Guys 2");
+        }
+    }
+}
+    class mainwala
+
+{
+     static void Main(string[] args)
+    {
+        first.goo G1 = new first.goo();
+        second.goo s = new second.goo();
+
+        G1.sayhello();
+        s.sayhello();
+
+    }
+}*/
---
+++ namespace 3.cs
@@ -0,0 +1,37 @@
+﻿/*
+
+using System;
+
+using first;
+using second;
+namespace first
+{
+    public class program
+    {
+        public void display()
+        {
+            Console.WriteLine("this is first program");
+        }
+    }
+}
+namespace second
+{
+    public class pro
+    {
+        public void display()
+        {
+            Console.WriteLine("this is second program");
+        }
+    }
+}class programplus
+{
+    static void Main()
+    {
+        program p1 = new program();
+        pro p2 = new pro();
+
+        p1.display();
+        p2.display();
+    }
+}*/
+
